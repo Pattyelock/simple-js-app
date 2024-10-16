7eafef268e6044e3cabdfd23eb755de13f06b3b2
pattylock <60874511+pattylock@users.noreply.github.com>
2024-10-16 15:20:04 +0200

    Add Pokémon objects list
    Add Pokémon objects to the array
    pokemonList.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
-alert('Hello world');
+// Create an empty array to store Pokémon data
+let pokemonList = [];
 
-const favoriteFood = 'Baked Salmon';
-document.write(favoriteFood);
+// Add Pokémon objects to the array
+pokemonList.push(
+  {
+    name: "Bulbasaur",
+    height: 7,
+    types: ["grass", "poison"]
+  },
+  {
+    name: "Charmander",
+    height: 6,
+    types: ["fire"]
+  },
+  {
+    name: "Squirtle",
+    height: 5,
+    types: ["water"]
+  }
+);
+
+// Log the pokemonList array to see the result
+console.log(pokemonList);
