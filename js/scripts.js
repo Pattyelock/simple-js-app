6df6c291acabb686546a91da9e5075596238d5dd
pattylock <60874511+pattylock@users.noreply.github.com>
2024-10-22 18:31:16 +0200

    pokemonRepository
    Created the pokemonRepository variable and assign the IIFE to it

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,37 @@
-// Create an empty array to store Pokémon data
-let pokemonList = [];
+// Create the pokemonRepository variable and assign the IIFE to it
+let pokemonRepository = (function() {
+  // The pokemonList array is now private within this IIFE
+  let pokemonList = [
+      { name: 'Pikachu', type: 'Electric', level: 10 },
+      { name: 'Charmander', type: 'Fire', level: 12 },
+      { name: 'Bulbasaur', type: 'Grass', level: 9 },
+      { name: 'Squirtle', type: 'Water', level: 11 }
+  ];
 
-// Add Pokémon objects to the array
-pokemonList.push(
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["grass", "poison"]
-  },
-  {
-    name: "Charizard",
-    height: 1.7,
-    types: ["fire", "flying"]
-  },
-  {
-    name: "Squirtle",
-    height: 1.0,
-    types: ["water"]
-  }
-);
-
-// Part 1: Create a for loop to iterate through the pokemonList array
-for (let i = 0; i < pokemonList.length; i++) {
-  // Create a variable to hold the output string for each Pokémon
-  let output = pokemonList[i].name + " (height: " + pokemonList[i].height + ")";
-  
-  // Part 2: Add a conditional to highlight large Pokémon
-  // If the Pokémon's height is greater than 1.5, add a "Wow, that's big!" note
-  if (pokemonList[i].height > 1.5) {
-    output += " - Wow, that's big!";
+  // Function to get all Pokémon (public)
+  function getAll() {
+      return pokemonList;
   }
 
-  // Use document.write() to write the result on the web page
-  document.write(output + "<br>");
-}
+  // Function to add a Pokémon to the list (public)
+  function add(pokemon) {
+      pokemonList.push(pokemon);
+  }
+
+  // The IIFE returns an object with references to the getAll and add functions
+  return {
+      getAll: getAll,
+      add: add
+  };
+})();
+
+// Use the getAll() method to access the pokemonList array and update the forEach() loop
+pokemonRepository.getAll().forEach(function(pokemon) {
+  console.log(`Name: ${pokemon.name}, Type: ${pokemon.type}, Level: ${pokemon.level}`);
+});
+
+// Example of adding a new Pokémon using the add function
+pokemonRepository.add({ name: 'Jigglypuff', type: 'Fairy', level: 8 });
+
+// Verify that the new Pokémon was added by printing the updated list
+console.log(pokemonRepository.getAll());
